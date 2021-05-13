042328c8521ef8593302848114826c877ce56f9b
gaurangvaidya <gaurang41098@gmail.com>
2021-05-13 22:02:30 +0530

    Assignment Submission Login Form

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  username="";
+  password="";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit():void {
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+    if(!emailRegex.test(this.username.toLowerCase()))
+    {
+
+        alert("Please Enter Valid Email");
+        return;
+
+    }
+
+    if(this.password.length<8)
+    {
+      alert("Please Enter Password Of Length of Atleast 8");
+      return;
+    }
+
+    alert("Logged In Successfully");
+
+
+
+  }
+
+}
